42a9483e3adc403f3135215dfbebe9392d001eb9
Rizal <doczal@gmail.com>
2019-05-15 23:28:26 +0800

    Combined reducers and archive action

--- src/index.js
+++ src/index.js
@@ -2,31 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import store from './store';
+import { STORY_ARCHIVE } from './constants/actionTypes';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-const stories = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  }, {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 ReactDOM.render(
   <App
-    stories={store.getState()}
-    onArchive={() => {}}
+    stories={store.getState().storyState}
+    onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
   />,
   document.getElementById('root')
 );
